9b83abbbe13b5ad4252f2ada2749352098d1dd0c
Yael <yaelppe@gmail.com>
2023-11-24 02:06:53 -0300

    WIP: starting scene where videos and text are visualized

--- Script/start_screen.gd
+++ Script/start_screen.gd
@@ -1,39 +1,40 @@
 extends Control
 
 
-@onready var primero = $Panel/Container/primero
-@onready var segundo = $Panel/Container/segundo
-@onready var tercero = $Panel/Container/tercero
-@onready var label = $Panel/Label
+@onready var container = $Container
+@onready var primero = $Container/primero
+@onready var segundo = $Container/segundo
+@onready var tercero = $Container/tercero
+@onready var label = $Label
 
 
-
-
-var full_text = "esto tiene que ser un texto que dure todo lo que dura el total de los tres videos de alguna manera necesitaria saberlo
-sigue hacia abajo o se rompe"
-var displayed_text = ""
+var fullText: int = 200  
 var text_speed = 0.05 # Tiempo en segundos entre letras
 var time_since_last_char = 0.0
+var char_index = 0 # Nuevo índice para controlar la adición de caracteres
 
- 
 func _ready():
-	label.text = ""
-	
+	get_tree().set_pause(true)
+	pass
+
 func _process(delta):
 	time_since_last_char += delta
-	if time_since_last_char >= text_speed and len(displayed_text) < len(full_text):
-		var next_char = full_text[len(displayed_text)]
-		displayed_text += next_char
-		label.text = displayed_text
+	if time_since_last_char >= text_speed and label.get_visible_characters() < fullText:
+		char_index += 1
+		label.set_visible_characters(char_index)
 		time_since_last_char = 0
 	pass
 
 
 func _on_video_stream_player_finished():
 	segundo.play()
-	pass # Replace with function body.
-
+	pass 
 
 func _on_segundo_finished():
 	tercero.play()
-	pass # Replace with function body.
+	pass
+
+func _on_tercero_finished():
+	get_tree().set_pause(false)
+	queue_free()
+	pass 
